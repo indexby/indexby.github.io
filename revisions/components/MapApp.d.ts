04f33ac7426352add444bb8d7bb02080b2b9f58e
indexby <iddqdsergey@gmail.com>
2024-08-08 11:35:38 +0000

    Update from https://github.com/indexby/revisions/commit/9bb94ff929675b5781dfed59fd49a8df391c889c

--- revisions/components/MapApp.d.ts
+++ revisions/components/MapApp.d.ts
@@ -2,5 +2,3 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "leaflet/dist/leaflet.css";
 export declare const MapApp: () => React.JSX.Element;
-export declare function get(obj: any, propPath: string, defaultValue?: any): any;
-export declare function getPropertyByPath(obj: any, propPath: string, defaultValue?: any): any;
---
+++ revisions/components/PlaceMarker.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+declare const PlaceMarker: ({ hit, setCurrentLocId }: any) => React.JSX.Element;
+export default PlaceMarker;
