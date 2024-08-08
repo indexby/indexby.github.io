d9e5d07a20e12f3126ae67041365349389834d3a
indexby <iddqdsergey@gmail.com>
2024-08-08 07:51:06 +0000

    Update from https://github.com/indexby/revisions/commit/26c5c00e9e357bf4b1e8ecc4fcb6a904471f4784

--- revisions/components/DraggableCard.d.ts
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export declare const DraggableCard: ({ family, indexFamily, debouncedSearchTerm, setCurrentFamily }: any) => React.JSX.Element;
--- revisions/components/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const plural: (number: number, titles?: Array<string>) => string;
-export declare function get(obj: any, propPath: string, defaultValue?: any): any;
-export declare function getPropertyByPath(obj: any, propPath: string, defaultValue?: any): any;
