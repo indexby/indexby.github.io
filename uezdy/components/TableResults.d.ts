ecc33defa51a8156f5c49a4f063faf8e5ec24679
indexby <iddqdsergey@gmail.com>
2024-08-16 19:58:51 +0000

    Update from https://github.com/indexby/uezdy/commit/93e6c1c0b29ac0d112e8de56a214eef838dc270b

--- uezdy/components/TableResults.d.ts
+++ uezdy/components/TableResults.d.ts
@@ -1,2 +1,2 @@
 import React from "react";
-export declare const TableResults: ({ hits }: any) => React.JSX.Element;
+export declare const TableResults: ({ hits, setIsShowPanel }: any) => React.JSX.Element;
