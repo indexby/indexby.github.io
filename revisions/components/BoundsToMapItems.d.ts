d09dd7d2edbcc008f7b4196ed40e4a933ecf15bc
indexby <iddqdsergey@gmail.com>
2024-08-27 12:01:56 +0000

    Update from https://github.com/indexby/revisions/commit/a4dbcaccac3aad849b62600b91bd624722acb15c

--- revisions/components/BoundsToMapItems.d.ts
+++ revisions/components/BoundsToMapItems.d.ts
@@ -1,3 +1,3 @@
 import React from "react";
-declare const BoundsToMapItems: ({ bounds, callback }: any) => React.JSX.Element;
+declare const BoundsToMapItems: ({ bounds, callback, mapBounds }: any) => React.JSX.Element;
 export default BoundsToMapItems;
