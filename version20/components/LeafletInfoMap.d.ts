7593878026a0dbd3b69acae99538dd7474e88559
indexby <iddqdsergey@gmail.com>
2024-11-20 12:38:50 +0000

    Update from https://github.com/shappoff/metrics/commit/db9d56c1f8c8ae4b60bf0b81cc364b0b11ebc3a9

---
+++ version20/components/LeafletInfoMap.d.ts
@@ -0,0 +1,3 @@
+import React from 'react';
+import "leaflet/dist/leaflet.css";
+export declare const LeafletInfoMap: React.FC<any>;
---
+++ version20/components/utils.d.ts
@@ -0,0 +1,2 @@
+export declare const getNickName: (email?: string) => string;
+export declare function throttle(func: Function, ms: number): () => void;
