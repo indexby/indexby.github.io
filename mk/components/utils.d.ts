0d2029262ff5ce98eeecb61aae9181186b561d23
indexby <iddqdsergey@gmail.com>
2024-12-01 20:29:50 +0000

    Update from https://github.com/indexby/metrics/commit/ff38144cec9eb7bd4019a3942a9603f65b29a98b

--- mk/components/utils.d.ts
+++ mk/components/utils.d.ts
@@ -1,2 +1,3 @@
 export declare const getNickName: (email?: string) => string;
 export declare function throttle(func: Function, ms: number): () => void;
+export declare function plural(number: number, titles?: Array<string>): string;
