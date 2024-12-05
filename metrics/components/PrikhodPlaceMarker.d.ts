ed2f9892e5806afde95de6358f3ec479605a8ab0
indexby <iddqdsergey@gmail.com>
2024-12-05 06:08:26 +0000

    Update from https://github.com/indexby/metrics/commit/6af9a9279ca9670b9658365189eba3f1f74ff761

---
+++ metrics/components/PrikhodPlaceMarker.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+declare const PrikhodPlaceMarker: ({ hit, onSelect }: any) => React.JSX.Element;
+export default PrikhodPlaceMarker;
---
+++ metrics/components/utils.d.ts
@@ -0,0 +1,4 @@
+export declare const getNickName: (email?: string) => string;
+export declare function throttle(func: Function, ms: number): () => void;
+export declare function plural(number: number, titles?: Array<string>): string;
+export declare function copyToClipboard(data: string): void;
