f536a4e6dc460404cd60943e150570694368cbbc
indexby <iddqdsergey@gmail.com>
2024-12-05 20:15:11 +0000

    Update from https://github.com/indexby/metrics/commit/6b7dcc5727af49bca63ab4135bdcfa5edbee1b6c

--- metrics/components/FilterBar.d.ts
+++ metrics/components/FilterBar.d.ts
@@ -1,2 +1,2 @@
 import React from "react";
-export default function FilterBar({ type, church, callBack }: any): React.JSX.Element;
+export default function FilterBar({ type, church, callBack, noMK }: any): React.JSX.Element;
